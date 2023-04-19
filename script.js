bbd327579382e192d34d11852ee4f1c4eb921435
nsamedaisy <nsamedaisy@gmail.com>
2023-04-19 15:14:57 +0100

    expanding card complete

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const image = document.querySelectorAll('imgs')
-image.forEach(imgs) {
-  imgs.addEventListener('click', () => {
+const image = document.querySelectorAll('.imgs');
 
+image.forEach(imgs => {
+  imgs.addEventListener('click', () => {
+    removeActiveClasses()
+    imgs.classList.add('active')
   })
-}
+})
+
+  function removeActiveClasses() {
+    image.forEach(imgs => {
+      imgs.classList.remove('active')
+    })
+  }
+
+// const anotherimage = document.querySelector('.container')
+// const newImage = document.createElement('.imgs');
+// const src = "./img6.jpeg";
+// anotherimage.appendChild(newImage);
